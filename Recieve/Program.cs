06aa4ef5e2629359d6eac194f036cdbd7a7285a9
Joachim Tyrfing Richter <joac0724@stud.kea.dk>
2024-05-08 21:19:13 +0200

    added: readme file for setting user secrets

--- Recieve/Program.cs
+++ Recieve/Program.cs
@@ -1,5 +1,9 @@
-﻿using System.Text;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS8622 // Nullability of reference types in type of parameter doesn't match the target delegate (possibly because of nullability attributes).
+
+using System.Text;
 using System.Text.Json;
+using Microsoft.Extensions.Configuration;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -7,81 +11,82 @@ namespace Receive;
 
 public class Program
 {
-    public static void Main()
+    private static IConfiguration _configuration;
+    private static IModel _channel;
+    private static IConnection _connection;
+
+    public static void Main(string[] args)
     {
-        var factory = new ConnectionFactory { HostName = "localhost" };
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddUserSecrets<Program>();
+        _configuration = builder.Build();
 
-        channel.QueueDeclare(queue: "hello",
-            durable: false,
-            exclusive: false,
-            autoDelete: false,
-            arguments: null);
+        var rabbitMqUrl = _configuration["RabbitMQ:RMQ_URL"];
+        var factory = new ConnectionFactory { Uri = new Uri(rabbitMqUrl) };
 
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+
+        _channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
         Console.WriteLine(" [*] Waiting for messages from CinemaAPI.");
 
-        var consumer = new EventingBasicConsumer(channel);
-        consumer.Received += (model, ea) =>
-        {
-            var body = ea.Body.ToArray();
-            var message = Encoding.UTF8.GetString(body);
-            try
-            {
-                using var doc = JsonDocument.Parse(message);
-                Console.WriteLine($" [x] Received {message}");
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += Consumer_Received;
 
-                ProcessData(doc.RootElement);
-                ForwardToAdminApi(doc.RootElement);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($" [!] Error processing message: {ex.Message}");
-                NotifyEmailApiAboutError(ex.Message);
-            }
-        };
-
-        channel.BasicConsume(queue: "hello",
-            autoAck: true,
-            consumer: consumer);
+        _channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
 
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
     }
 
-    private static void ProcessData(JsonElement data)
+    private static void Consumer_Received(object model, BasicDeliverEventArgs ea)
     {
         try
         {
-            throw new Exception("Simulated transaction failure.");
-
-            // Code to process transaction normally
-            Console.WriteLine("Processing transaction...");
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            using var doc = JsonDocument.Parse(message);
+            Console.WriteLine($" [x] Received {message}");
+            ProcessData(doc.RootElement);
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error occurred: {ex.Message}");
+            Console.WriteLine($" [!] Error processing message: {ex.Message}");
             NotifyEmailApiAboutError(ex.Message);
+        }
+    }
+
+    private static void ProcessData(JsonElement data)
+    {
+        Console.WriteLine("Processing transaction...");
+        var random = new Random();
+        var chance = random.Next(100);
+
+        if (chance < 70)
+        {
+            ForwardToAdminApi(data);
+        }
+        else
+        {
+            NotifyEmailApiAboutError("Simulated message based on data processing decision.");
         }
     }
 
     private static void ForwardToAdminApi(JsonElement data)
     {
-        // TODO: Create code to forward data to AdminAPI
         Console.WriteLine("Forwarding data to AdminAPI...");
+        var message = JsonSerializer.Serialize(data);
+        var body = Encoding.UTF8.GetBytes(message);
+        _channel.BasicPublish(exchange: "", routingKey: "successQueue", basicProperties: null, body: body);
+        Console.WriteLine(" [x] Data sent to AdminAPI");
     }
 
     private static void NotifyEmailApiAboutError(string errorMessage)
     {
-        var factory = new ConnectionFactory() { HostName = "localhost" };
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
+        Console.WriteLine("Notifying error...");
         var body = Encoding.UTF8.GetBytes(errorMessage);
-
-        channel.BasicPublish(exchange: "",
-            routingKey: "errorQueue",
-            basicProperties: null,
-            body: body);
+        _channel.BasicPublish(exchange: "", routingKey: "errorQueue", basicProperties: null, body: body);
         Console.WriteLine(" [x] Sent {0}", errorMessage);
     }
 }
